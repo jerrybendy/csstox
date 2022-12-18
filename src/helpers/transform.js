a850ba57b28a10e43143de60363994d61f3ac243
Jerry Bendy <jerry@icewingcc.com>
2022-12-18 22:35:51 +0800

    transform font-weight to font-family for RN

--- src/helpers/transform.js
+++ src/helpers/transform.js
@@ -19,8 +19,30 @@ export const toRN = (cssText) => {
   try {
     const output = toJSSObject(cssText);
     const result = Object.keys(output).map((rules) => [rules, output[rules]]);
-    return JSON.stringify(transform(result), null, 2);
+    const transformResult = transformFontFamily(transform(result));
+    console.log(transformResult);
+
+    return JSON.stringify(transformResult, null, 2);
   } catch (e) {
     return "Error translating CSS to RN";
   }
 };
+
+const fontWeightSuffix = {
+  "100": "Thin",
+  "300": "Light",
+  "400": "Regular",
+  "normal": "Regular",
+  "500": "Medium",
+  "700": "Bold",
+  "bold": "Bold",
+  "900": "Black",
+}
+
+function transformFontFamily(styles) {
+  if (styles.fontFamily && styles.fontWeight) {
+    styles.fontFamily += `-${fontWeightSuffix[styles.fontWeight]}`;
+    delete styles.fontWeight;
+  }
+  return styles;
+}
